ccb8df290148447105a1d03fb6694fc0c14524c0
davidandrus1 <david.andrus@stud.ubbcluj.ro>
2025-10-20 14:21:16 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -41,6 +41,23 @@ class Main {
         return maxSum;
     }
 
+    public static int calculateMinSum(int[] array){
+        int minSum = 0;
+//        for (int i = 0; i < array.length - 1; i++) {
+//            if (array[i] > array[i + 1]) {
+//                int aux = array[i];
+//                array[i] = array[i + 1];
+//                array[i + 1] = aux;
+//            }
+//        }
+        Arrays.sort(array);
+        for(int i = 0; i < array.length - 1; i++){
+            minSum = minSum + array[i];
+        }
+
+        return minSum;
+    }
+
 
     public static void main(String args[]){
         Scanner input = new Scanner(System.in);
@@ -62,6 +79,8 @@ class Main {
 
         int maxSum = calculateMaxSum(array);
         System.out.println("Die maximale Summe ist : "+maxSum);
+        int minSum = calculateMinSum(array);
+        System.out.println("Die minimale Summe ist : "+minSum);
 
     }
 }
